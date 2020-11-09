80e041f6cf64755c531f27da362fdc42d3757460
linse073 <linse073@gmail.com>
2020-11-09 15:14:47 +0800

    flock compile error

--- 3rd/flock.mk
+++ 3rd/flock.mk
@@ -1,19 +1,19 @@
-SRCS	= $(wildcard ./*.cpp)
-SRCS	+= $(wildcard ./Asset/*.cpp)
-SRCS	+= $(wildcard ./Bullet/*.cpp)
-SRCS	+= $(wildcard ./Engine/*.cpp)
-SRCS	+= $(wildcard ./FixedPoint/*.cpp)
-SRCS	+= $(wildcard ./Flocking/*.cpp)
-SRCS	+= $(wildcard ./Flocking/Behavior/*.cpp)
-SRCS	+= $(wildcard ./Flocking/Shap/*.cpp)
-OBJS	= $(patsubst ./%.cpp, ./%.o, $(SRCS))
-INCS	= -I./Asset
-INCS	+= -I./Bullet
-INCS	+= -I./Engine
-INCS	+= -I./FixedPoint
-INCS	+= -I./Flocking
-INCS	+= -I./Flocking/Behavior
-INCS	+= -I./Flocking/Shap
+SRCS	= $(wildcard *.cpp)
+SRCS	+= $(wildcard Asset/*.cpp)
+SRCS	+= $(wildcard Bullet/*.cpp)
+SRCS	+= $(wildcard Engine/*.cpp)
+SRCS	+= $(wildcard FixedPoint/*.cpp)
+SRCS	+= $(wildcard Flocking/*.cpp)
+SRCS	+= $(wildcard Flocking/Behavior/*.cpp)
+SRCS	+= $(wildcard Flocking/Shap/*.cpp)
+OBJS	= $(patsubst %.cpp, %.o, $(SRCS))
+INCLUDE	= -IAsset
+INCLUDE	+= -IBullet
+INCLUDE	+= -IEngine
+INCLUDE	+= -IFixedPoint
+INCLUDE	+= -IFlocking
+INCLUDE	+= -IFlocking/Behavior
+INCLUDE	+= -IFlocking/Shap
 LIBS 	= lflock.so
 
 include ../cpp_common.mk
