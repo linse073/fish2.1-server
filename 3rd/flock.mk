63af1654cfe73b53cf5e25d25a82b5935eb2b391
linse073 <linse073@gmail.com>
2020-11-09 15:11:27 +0800

    flock compile error

--- 3rd/flock.mk
+++ 3rd/flock.mk
@@ -1,19 +1,19 @@
-SRCS	= $(wildcard *.cpp)
-SRCS	+= $(wildcard Asset/*.cpp)
-SRCS	+= $(wildcard Bullet/*.cpp)
-SRCS	+= $(wildcard Engine/*.cpp)
-SRCS	+= $(wildcard FixedPoint/*.cpp)
-SRCS	+= $(wildcard Flocking/*.cpp)
-SRCS	+= $(wildcard Flocking/Behavior/*.cpp)
-SRCS	+= $(wildcard Flocking/Shap/*.cpp)
-OBJS	= $(patsubst %.cpp, %.o, $(SRCS))
-INCS	= -IAsset
-INCS	+= -IBullet
-INCS	+= -IEngine
-INCS	+= -IFixedPoint
-INCS	+= -IFlocking
-INCS	+= -IFlocking/Behavior
-INCS	+= -IFlocking/Shap
+SRCS	= $(wildcard ./*.cpp)
+SRCS	+= $(wildcard ./Asset/*.cpp)
+SRCS	+= $(wildcard ./Bullet/*.cpp)
+SRCS	+= $(wildcard ./Engine/*.cpp)
+SRCS	+= $(wildcard ./FixedPoint/*.cpp)
+SRCS	+= $(wildcard ./Flocking/*.cpp)
+SRCS	+= $(wildcard ./Flocking/Behavior/*.cpp)
+SRCS	+= $(wildcard ./Flocking/Shap/*.cpp)
+OBJS	= $(patsubst ./%.cpp, ./%.o, $(SRCS))
+INCS	= -I./Asset
+INCS	+= -I./Bullet
+INCS	+= -I./Engine
+INCS	+= -I./FixedPoint
+INCS	+= -I./Flocking
+INCS	+= -I./Flocking/Behavior
+INCS	+= -I./Flocking/Shap
 LIBS 	= lflock.so
 
 include ../cpp_common.mk
