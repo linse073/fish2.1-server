bd6a3ddbbab02bd3a1eee5c2611122d54bc04146
linse073 <linse073@gmail.com>
2020-11-09 15:02:50 +0800

    build flock

--- shell/build.sh
+++ shell/build.sh
@@ -23,6 +23,11 @@ function build() {
     make -f ../kcp.mk
 
     echo "====================="
+    echo "start build lua-flock..."
+    cd $basepath/3rd/flock
+    make -f ../flock.mk
+
+    echo "====================="
     echo "clean..."
     cd $basepath
     find . -name "*.o"  | xargs rm -f
