2e86cc6b5c8dcd702758e7c839a2beff231ab346
linse073 <linse073@gmail.com>
2020-11-15 11:58:22 +0800

    test game

---
+++ test/service/test_game.lua
@@ -0,0 +1,21 @@
+local skynet_m = require "skynet_m"
+
+skynet_m.start(function()
+    skynet_m.log("Server start.")
+
+    -- debug service
+    if not skynet_m.getenv("daemon") then
+        skynet_m.newservice("console")
+    end
+
+    -- service
+    skynet_m.uniqueservice("routine")
+    skynet_m.uniqueservice("gate_mgr")
+    skynet_m.uniqueservice("room_mgr")
+    local game_client = skynet_m.uniqueservice("game_client")
+    skynet_m.send_lua(game_client, "start")
+
+    skynet_m.log("Server start finish.")
+
+    skynet_m.exit()
+end)
