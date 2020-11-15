76d105eae6ca80b13273b0422b379e4b928a819a
linse073 <linse073@gmail.com>
2020-11-15 21:06:09 +0800

    test_loadstring error

--- test/service/test_loadstring.lua
+++ test/service/test_loadstring.lua
@@ -8,7 +8,7 @@ skynet_m.start(function()
     local f = assert(io.open("./test/lib/send_msg.lua", "r"))
     local d = f:read("*a")
     f:close()
-    load(d)()
+    load(d)()()
 
     skynet_m.exit()
 end)
