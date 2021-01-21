be93ff8b77e1bdd9e882ea48f17634eec2e3217d
linse073 <linse073@gmail.com>
2021-01-21 22:31:49 +0800

    modify fish data

--- fish/data/fish_data.lua
+++ fish/data/fish_data.lua
@@ -1,22 +1,22 @@
 local data = {
 	[5000] = {
-		type = 0,
+		type = 1,
 		life_time = 0.000000,
 	},
 	[5001] = {
-		type = 0,
+		type = 1,
 		life_time = 0.000000,
 	},
 	[5002] = {
-		type = 0,
+		type = 1,
 		life_time = 0.000000,
 	},
 	[5003] = {
-		type = 1,
+		type = 2,
 		life_time = 0.000000,
 	},
 	[5004] = {
-		type = 1,
+		type = 2,
 		life_time = 0.000000,
 	},
 }
