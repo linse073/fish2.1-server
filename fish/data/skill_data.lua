7c5cbf11e479fd8963bc8ef1df535e37795aa1cc
linse073 <linse073@gmail.com>
2021-02-02 12:12:36 +0800

    fffff

--- fish/data/skill_data.lua
+++ fish/data/skill_data.lua
@@ -4,7 +4,7 @@ local data = {
         born_time = 15,      --boss出生到第一个技能开始时间
         skill = {
             {
-                duration = 50, --技能持续时间
+                duration = 30, --技能持续时间
 				hit_count = 1, --被命中几次打断技能
 				fish = {
 					{
@@ -167,7 +167,7 @@ local data = {
 				},
             },
             {
-                duration = 50,
+                duration = 30,
 				hit_count = 3, --被命中几次打断技能
 				fish = {
 					{
@@ -200,7 +200,7 @@ local data = {
 				},
             },
             {
-                duration = 50,
+                duration = 30,
 				hit_count = 2, --被命中几次打断技能
 				fish = {
 					{
@@ -224,7 +224,7 @@ local data = {
 				},
             },
             {
-                duration = 50,
+                duration = 30,
 				hit_count = 1, --被命中几次打断技能
 				fish = {
 					{
