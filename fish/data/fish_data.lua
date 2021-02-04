70ebeb628801976bd408331d11412080445c69d8
linse073 <linse073@gmail.com>
2021-02-04 20:46:07 +0800

    update data

--- fish/data/fish_data.lua
+++ fish/data/fish_data.lua
@@ -2,146 +2,182 @@ local data = {
 	[5000] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5001] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5002] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5003] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5004] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5005] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5006] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5007] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5008] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5009] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5010] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5011] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5100] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5101] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5102] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5103] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5104] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5105] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5106] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5107] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5108] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5109] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5110] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5111] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5112] = {
 		type = 1,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5502] = {
 		type = 2,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5503] = {
 		type = 2,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[5504] = {
 		type = 2,
 		life_time = 0.000000,
+		frozen_immune = false,
 	},
 	[1001] = {
 		type = 3,
 		life_time = 240.000000,
+		frozen_immune = true,
 	},
 	[3000] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3001] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3002] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3003] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3004] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3005] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 	[3006] = {
 		type = 1,
 		life_time = 100.000000,
+		frozen_immune = false,
 	},
 }
 
