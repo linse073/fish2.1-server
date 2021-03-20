6e6d54582b0a114d16f28fa9217b274e8ec480b4
linse073 <linse073@gmail.com>
2021-03-20 13:29:38 +0800

    update data

--- fish/data/fish_born.lua
+++ fish/data/fish_born.lua
@@ -7,24 +7,24 @@ local data = {
         --     rand_max = 1,
         --     matrix_id = 0,
         -- },
-        {
-            fish_id = 5556,
-            rand_min = 1,
-            rand_max = 1,
-            matrix_id = 0,
-        },
+        -- {
+        --     fish_id = 5556,
+        --     rand_min = 1,
+        --     rand_max = 1,
+        --     matrix_id = 0,
+        -- },
         -- {
         --     fish_id = 5557,
         --     rand_min = 1,
         --     rand_max = 1,
         --     matrix_id = 0,
         -- },
-        -- {
-        --     fish_id = 5558,
-        --     rand_min = 1,
-        --     rand_max = 1,
-        --     matrix_id = 0,
-        -- },
+        {
+            fish_id = 5558,
+            rand_min = 1,
+            rand_max = 1,
+            matrix_id = 0,
+        },
     }
 }
 
