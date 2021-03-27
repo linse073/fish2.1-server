7dd1054c2ae4ad0aa0ab221fce1207a14f6ca9e3
linse073 <linse073@gmail.com>
2021-03-27 18:02:04 +0800

    update data

--- fish/data/skill_data.lua
+++ fish/data/skill_data.lua
@@ -250,57 +250,6 @@ local data = {
 				},
             },
         },
-		-- {
-		-- 	duration = 20, -- 技能持续时间
-		-- 	hit_count = 1, -- 被命中几次打断技能
-		-- 	fish_id = 3000,
-		-- 	damage_count = 2,
-		-- 	fish = {
-		-- 		{
-		-- 			fish_id = 3000,
-		-- 			spline_id = 0,
-		-- 			time = 3, -- 多少秒可以刷这个鱼
-		-- 			in_count = true,
-		-- 			num = 1, -- 多个boss目标点
-		-- 			matrix_id = 0, -- 鱼阵列ID
-		-- 			speed = 0,
-		-- 		},
-		-- 	},
-		-- },
-		-- {
-		-- 	duration = 20, -- 技能持续时间
-		-- 	hit_count = 1, -- 被命中几次打断技能
-		-- 	fish_id = 3000,
-		-- 	damage_count = 2,
-		-- 	fish = {
-		-- 		{
-		-- 			fish_id = 3000,
-		-- 			spline_id = 0,
-		-- 			time = 3, -- 多少秒可以刷这个鱼
-		-- 			in_count = true,
-		-- 			num = 1, -- 多个boss目标点
-		-- 			matrix_id = 0, -- 鱼阵列ID
-		-- 			speed = 0,
-		-- 		},
-		-- 	},
-		-- },
-		-- {
-		-- 	duration = 20, -- 技能持续时间
-		-- 	hit_count = 1, -- 被命中几次打断技能
-		-- 	fish_id = 0,
-		-- 	damage_count = 2,
-		-- 	fish = {
-		-- 		{
-		-- 			fish_id = 3000,
-		-- 			spline_id = 0,
-		-- 			time = 3, -- 多少秒可以刷这个鱼
-		-- 			in_count = true,
-		-- 			num = 1, -- 多个boss目标点
-		-- 			matrix_id = 0, -- 鱼阵列ID
-		-- 			speed = 0,
-		-- 		},
-		-- 	},
-		-- },
 	},
 	[1006] = {
 		born_time = 10,
