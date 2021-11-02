9f051dff90577f640633fdfaddc16e02ef37e383
linse073 <linse073@gmail.com>
2021-11-02 19:58:38 +0800

    get all proto type

--- fish/service/cache.lua
+++ fish/service/cache.lua
@@ -7,7 +7,7 @@ local ipairs = ipairs
 local pairs = pairs
 
 local function proto_map(pre, sp)
-    local all_proto = sp:allprototype()
+    local all_proto = sp:all_type()
     local i2n, n2i = {}, {}
     for k, v in ipairs(all_proto) do
         i2n[k] = v
