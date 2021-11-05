a9c8e7fbac5f29ca6c5903789be7e66cd1b24bad
linse073 <linse073@gmail.com>
2021-11-05 10:36:29 +0800

    push shell

--- push/push.cmd
+++ push/push.cmd
@@ -1,4 +1,4 @@
-scp -i ./id_rsa.by ../fish/lib/* ubuntu@146.56.234.157:~/fish01/fish-server/fish/lib/
-scp -i ./id_rsa.by ../fish/data/*.lua ubuntu@146.56.234.157:~/fish01/fish-server/fish/data
-scp -i ./id_rsa.by ../fish/service/* ubuntu@146.56.234.157:~/fish01/fish-server/fish/service
-ssh -i ./id_rsa.by ubuntu@146.56.234.157 "~/server_shell_01/restart.sh"
+scp -i ./id_rsa.by ../fish/lib/* ubuntu@146.56.234.157:~/fish02/fish2-server/fish/lib/
+scp -i ./id_rsa.by ../fish/data/*.lua ubuntu@146.56.234.157:~/fish02/fish2-server/fish/data
+scp -i ./id_rsa.by ../fish/service/* ubuntu@146.56.234.157:~/fish02/fish2-server/fish/service
+ssh -i ./id_rsa.by ubuntu@146.56.234.157 "~/server_shell_02/restart.sh"
--- push/server_shell_02/restart.sh
+++ push/server_shell_02/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pgrep -f config_02 | xargs kill
-cd ~/fish01/fish-server/shell
+cd ~/fish02/fish2-server/shell
 sleep 0.1s
 ./run_02.sh
